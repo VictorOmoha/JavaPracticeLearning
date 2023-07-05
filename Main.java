528934b8259ca2a7bb54f633c98ef37f0e7ae8ea
Victor Omoha <64688748+VictorOmoha@users.noreply.github.com>
2023-07-05 14:08:38 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+       Person person = new Person();
+        System.out.println("name: " + person.name);
+        System.out.println("age: " + person.age);
+        System.out.println("address: " + person.address);
+        //System.out.println("ssn: " + person.ssn);
+        // can't access ssn
+    }
+}
