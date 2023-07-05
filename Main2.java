81f960debad89e58eb69a90b2a3f57f9a30d9d3c
Victor Omoha <64688748+VictorOmoha@users.noreply.github.com>
2023-07-05 14:09:42 -0400

    Add files via upload

---
+++ Main2.java
@@ -0,0 +1,30 @@
+public class Main2 {
+    public static void main(String[] args) {
+       /* int[] numbers = {5,4,6,7,9};
+
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        int n = 8;
+        int counter = 0;
+
+
+        while(counter < n){
+
+                System.out.println(counter * 2);
+                counter++;
+
+
+        }
+
+    }*/
+
+        int n = 8;
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= n);
+    }
+}
