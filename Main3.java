085d7dcc37db87da3232a4f08ffa0253560af2bf
Victor Omoha <64688748+VictorOmoha@users.noreply.github.com>
2023-07-06 12:42:51 -0400

    Add files via upload

---
+++ Main3.java
@@ -0,0 +1,16 @@
+public class Main3 {
+    public static void main(String[] args) {
+        int[] numbers = {8, 10, 12, 7, 4};
+        int sum = 0;
+
+        for (int i : numbers) {
+
+                sum += i;
+
+
+
+        }
+        System.out.println(sum);
+
+    }
+}
