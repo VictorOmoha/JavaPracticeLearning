234d218f40de5621f4d75c108d2a2cbb91dc521c
Victor Omoha <64688748+VictorOmoha@users.noreply.github.com>
2023-07-10 12:38:54 -0400

    Add files via upload

---
+++ Main5.java
@@ -0,0 +1,17 @@
+public class Main5 {
+    public static void main(String[] args) {
+        int[] numbers = {4,7,12,5,8};
+        int sumOdd = 0;
+        int count = 0;
+
+        for(int i: numbers){
+            if(i % 2 != 0){
+                sumOdd += i;
+                count++;
+            }
+
+        }
+        double aveOdd = (double) sumOdd / count;
+        System.out.println(aveOdd);
+    }
+}
