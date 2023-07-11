9681ca8fb8293011708e8de725ab80f2f0b66c00
Victor Omoha <64688748+VictorOmoha@users.noreply.github.com>
2023-07-11 13:03:26 -0400

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,11 +1,8 @@
+package objectsContainer;
+
 public class Person {
-
-    public String name = "Victor";
-    protected int age = 45;
-     String address = "5577 west gore rd";
-
-    private int ssn = 8888;
-
+    public String name;
 
 
 }
+
